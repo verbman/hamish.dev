bddbb019fae8a50a62d500a86587339e9b0bd510
Hamish Fraser <hamish@verb.co.nz>
2022-11-06 17:18:18 +1300

    Fix iphone nav button layout issue

--- components/HeadBar.tsx
+++ components/HeadBar.tsx
@@ -6,7 +6,7 @@ export default function HeadBar({ mode }: { mode: boolean }) {
   let svgfill = mode ? "#FFF" : "#111";
 
   return (<nav className={`navbar navbar-expand-lg ` + classes + ` fixed-top topnavigation nav-down`}>
-    <div className="container pe-5">
+    <div className="container pe-lg-5">
       <Link className="navbar-brand" aria-label="Home" href="/">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="40" height="40"><path fill="none" d="M0 0h24v24H0z" />
           <path d="M19 21H5a1 1 0 0 1-1-1v-9H1l10.327-9.388a1 1 0 0 1 1.346 0L23 11h-3v9a1 1 0 0 1-1 1zm-6-2h5V9.157l-6-5.454-6 5.454V19h5v-6h2v6z" fill={svgfill} />
